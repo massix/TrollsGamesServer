4be84e198842691e29f6ff46692954420f39fe2f
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-10-22 14:54:07 +0200

    Set headers when sending request. (Fix #6)

--- src/main/java/rocks/massi/services/BGGJsonProxy.java
+++ src/main/java/rocks/massi/services/BGGJsonProxy.java
@@ -1,5 +1,6 @@
 package rocks.massi.services;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 import rocks.massi.data.bgg.BGGGame;
@@ -7,8 +8,14 @@ import rocks.massi.data.bgg.Collection;
 
 public interface BGGJsonProxy {
     @RequestLine("GET /collection/{nick}")
+    @Headers(value = {
+            "User-Agent: TrollsGamesServer. Contact massimo.gengarelli@gmail.com"
+    })
     Collection getCollectionForUser(@Param("nick") final String user);
 
     @RequestLine("GET /thing/{id}")
+    @Headers(value = {
+            "User-Agent: TrollsGamesServer. Contact massimo.gengarelli@gmail.com"
+    })
     BGGGame getGameForId(@Param("id") final int gameId);
 }
