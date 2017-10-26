18560989c70ca4238f88f537f2b9800cc4b62914
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-10-26 21:13:16 +0200

    Fix typo in script.

--- configuration/deploy.sh
+++ configuration/deploy.sh
@@ -5,7 +5,7 @@ DEPLOY_ENV="$1"
 function deploy_files() {
     local now=$(date '+%Y-%m-%d.%H:%M')
     echo "Deploying new Jar with date '${now}'"
-    scp ${TRAVIS_BUILD_DIR}/target/TrollsGames*.jar prod:trolls-${DEPLOY_ENV}/archives/TrollsGames-${now}.jar
+    scp ${TRAVIS_BUILD_DIR}/target/TrollsGames*.jar prod:trolls-${DEPLOY_ENV}/archive/TrollsGames-${now}.jar
     scp ${TRAVIS_BUILD_DIR}/target/TrollsGames*.jar prod:trolls-${DEPLOY_ENV}/java/TrollsGames.jar
     scp ${TRAVIS_BUILD_DIR}/configuration/docker-compose-${DEPLOY_ENV}.yml prod:trolls-${DEPLOY_ENV}/
 }
