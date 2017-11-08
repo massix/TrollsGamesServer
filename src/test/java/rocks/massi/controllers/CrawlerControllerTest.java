51540132925c5c5b13a52cf9c20651a39feee60c
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-11-08 23:07:41 +0100

    I hate WireMock. Increase Timeouts.

--- src/test/java/rocks/massi/controllers/CrawlerControllerTest.java
+++ src/test/java/rocks/massi/controllers/CrawlerControllerTest.java
@@ -97,7 +97,7 @@ public class CrawlerControllerTest {
         // Wait for the Q to be over.
         ResponseEntity<CrawlingProgress[]> progress = restTemplate.getForEntity("/v1/crawler/queues", CrawlingProgress[].class);
         while (progress.getBody()[0].isRunning()) {
-            Thread.sleep(500);
+            Thread.sleep(2000);
             progress = restTemplate.getForEntity("/v1/crawler/queues", CrawlingProgress[].class);
             log.info("Still running {}", progress.getBody());
         }
@@ -111,6 +111,7 @@ public class CrawlerControllerTest {
         assertTrue(responseEntity.getHeaders().get("location").get(0).startsWith("/v1/crawler/queue"));
 
         waitForQ();
+        Thread.sleep(1000);
 
         // Check that honors have been inserted in the base
         Honor honor = honorsRepository.findById(19901);
@@ -152,6 +153,7 @@ public class CrawlerControllerTest {
 
         waitForQ();
 
+        Thread.sleep(1000);
         assertEquals(2, ownershipsRepository.findByUser("timed_user").size());
         restTemplate.delete("/v1/crawler/queues");
     }
@@ -161,10 +163,12 @@ public class CrawlerControllerTest {
         restTemplate.postForEntity("/v1/crawler/collection/bgg_user", null, Void.class);
         waitForQ();
 
+        Thread.sleep(1000);
         restTemplate.delete("/v1/crawler/queues");
         restTemplate.postForEntity("/v1/crawler/collection/bgg_user_two", null, Void.class);
         waitForQ();
 
+        Thread.sleep(1000);
         assertEquals(2, ownershipsRepository.findByUser("bgg_user").size());
         assertEquals(2, ownershipsRepository.findByUser("bgg_user_two").size());
     }
