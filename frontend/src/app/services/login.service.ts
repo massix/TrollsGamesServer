3f7fe7740fae8d3c6119204b4b2b447f56a0ecff
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-11-19 11:18:19 +0100

    Show a random quote at login

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { Login } from '../data/login';
 import { User } from '../data/user';
 import { environment } from '../../environments/environment';
+import { Quote } from '../data/quote';
 
 @Injectable()
 export class LoginService {
@@ -11,5 +12,9 @@ export class LoginService {
 
     login(login: Login): Observable<any> {
         return this.httpClient.post<User>(environment.apiBase + '/v1/users/login', login, {observe: 'response'});
+    }
+
+    quote(): Observable<Quote> {
+        return this.httpClient.get<Quote>(environment.apiBase + '/v1/server/quote');
     }
 }
