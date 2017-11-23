2643e35614cf968163195090331f8d9b8a9f6478
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-11-23 20:52:28 +0100

    Add Quotes controller.

---
+++ src/main/java/rocks/massi/controllers/QuotesController.java
@@ -0,0 +1,61 @@
+package rocks.massi.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rocks.massi.authentication.Role;
+import rocks.massi.authentication.TrollsJwt;
+import rocks.massi.data.Quote;
+import rocks.massi.data.QuotesRepository;
+import rocks.massi.exceptions.AuthenticationException;
+
+import java.util.List;
+import java.util.Random;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/quotes")
+public class QuotesController {
+
+    @Autowired
+    private final QuotesRepository quotesRepository;
+
+    @Autowired
+    private final TrollsJwt trollsJwt;
+
+    @CrossOrigin
+    @GetMapping("/get/random")
+    public Quote getRandomQuote() {
+        List<Quote> quotes = quotesRepository.findAll();
+        return quotes.get(new Random().nextInt(quotes.size()));
+    }
+
+    @CrossOrigin(allowedHeaders = {"Authorization"})
+    @PutMapping("/add")
+    public Quote addQuote(@RequestHeader("Authorization") String authorization, @RequestBody Quote quote) {
+        TrollsJwt.UserInformation userInformation = trollsJwt.getUserInformationFromToken(authorization);
+        if (userInformation.getRole() != Role.ADMIN) {
+            throw new AuthenticationException("User not authorized.");
+        }
+
+        quotesRepository.save(quote);
+        return quote;
+    }
+
+    @CrossOrigin(allowedHeaders = {"Authorization"})
+    @DeleteMapping("/remove")
+    public Quote removeQuote(@RequestHeader("Authorization") String authorization, @RequestBody Quote quote) {
+        if (trollsJwt.getUserInformationFromToken(authorization).getRole() != Role.ADMIN) {
+            throw new AuthenticationException("User not authorized.");
+        }
+
+        quotesRepository.delete(quote);
+        return quote;
+    }
+
+    @CrossOrigin
+    @GetMapping("/get")
+    public List<Quote> getAllQuotes() {
+        return quotesRepository.findAll();
+    }
+}
