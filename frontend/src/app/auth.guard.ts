b9b0e47c817ee2765469b4b3fee89c5ba16be700
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-12-02 09:55:05 +0100

    Missing an else condition

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -9,7 +9,7 @@ export class AuthGuard implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
         return new Promise((resolve) => {
             if (localStorage.getItem('token')) {
-                const resp = this.crawlService.getStatus().subscribe(
+                this.crawlService.getStatus().subscribe(
                     ok => {
                         console.log('Auth guard resolving to OK');
                         resolve(true);
@@ -21,6 +21,11 @@ export class AuthGuard implements CanActivate {
                         this.alertService.error('You are not logged in!', true);
                         resolve(false);
                 });
+            } else {
+                console.log('User not logged in');
+                this.router.navigate(['/login']);
+                this.alertService.error('You are not logged in!', true);
+                resolve(false);
             }
         });
     }
