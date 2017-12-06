9c5ff9f87290a38a41574c77cbb29bc9a1cc0830
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-12-06 19:37:39 +0100

    Implement endpoint to get user information

--- src/test/java/rocks/massi/controllers/utils/AuthorizationHandler.java
+++ src/test/java/rocks/massi/controllers/utils/AuthorizationHandler.java
@@ -22,4 +22,16 @@ public class AuthorizationHandler {
                     return execution.execute(request, body);
                 }));
     }
+
+    public static void setUpNormalUser(TestRestTemplate restTemplate, LoginInformation loginInformation) {
+        final ResponseEntity<User> authorizedUser = restTemplate.postForEntity("/v1/users/login", loginInformation, User.class);
+        assertTrue(authorizedUser.getStatusCode().is2xxSuccessful());
+
+        restTemplate.getRestTemplate().setInterceptors(
+                Collections.singletonList((request, body, execution) -> {
+                    request.getHeaders()
+                            .add("Authorization", authorizedUser.getHeaders().get("Authorization").get(0));
+                    return execution.execute(request, body);
+                }));
+    }
 }
