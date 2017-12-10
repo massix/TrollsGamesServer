c9fc1bf0188456577a974c18d0edcf2981bca20c
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-12-10 16:14:15 +0100

    Fix frontend.sh

--- configuration/frontend.sh
+++ configuration/frontend.sh
@@ -5,7 +5,18 @@ DEPLOY_ENV="$1"
 function build_backoffice() {
     cd frontend
     npm install
-    node_modules/@angular/cli/bin/ng build --prod -e ${DEPLOY_ENV}
+    local BUILD_ENV="prod"
+
+    case ${DEPLOY_ENV} in
+        develop)
+            BUILD_ENV="staging"
+            ;;
+        *)
+            BUILD_ENV="dev"
+            ;;
+    esac
+    echo "Building for ${BUILD_ENV}"
+    node_modules/@angular/cli/bin/ng build --prod -e ${BUILD_ENV}
 }
 
 build_backoffice
